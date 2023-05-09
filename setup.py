ccd65dd67ad43383d530bdbdfe109690dc9409da
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2023-05-09 00:53:05 -0300

    add build info

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ if __name__ == '__main__':
         description="A Python wrapper for whisper.cpp",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        version="0.1.8",
+        version="0.1.9",
         author="Carlos Cardoso Dias",
         author_email="carlosdias.dev@gmail.com",
         license="MIT",
